75ea77258882f1ca6efcbb448cce1e6e586f03f8
tizianocarranza <tizianocarranza0910@gmail.com>
2024-09-11 11:26:13 -0300

    trailing slash and build format added

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+
+  build: {
+    format: "file"
+  },
+
+  trailingSlash: "never",
+
   site: "https://my-portfolio-delta-one-61.vercel.app"
 });
