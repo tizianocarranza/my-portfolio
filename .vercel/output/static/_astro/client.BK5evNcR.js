61d3385256220e24977ce33c608ddc0e3b1001fd
tizianocarranza <tizianocarranza0910@gmail.com>
2024-09-11 18:41:17 -0300

    Vercel adapter added

---
+++ .vercel/output/static/_astro/client.BK5evNcR.js
@@ -0,0 +1,1 @@
+import{c as f}from"./client.DiD_DHAD.js";export{f as default};
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
+/* import vercel from "@astrojs/vercel/serverless"; */
+import vercel from "@astrojs/vercel/static"
+
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  output: "static",
+  adapter: vercel(),
 });
 
 
