64daa9c85365e7454ab357f83025ae717a41a41f
tizianocarranza <tizianocarranza0910@gmail.com>
2024-09-11 11:42:58 -0300

    vercel adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,18 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
 
-  build: {
+/*   build: {
     format: "file"
   },
 
   trailingSlash: "never",
-
-  site: "https://my-portfolio-delta-one-61.vercel.app"
+  site: "https://my-portfolio-delta-one-61.vercel.app", */
+  output: "server",
+  adapter: vercel()
 });
