1c3c84e03c88317f768a9b20c300333566986117
tizianocarranza <tizianocarranza0910@gmail.com>
2024-09-11 18:48:06 -0300

    Output to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
-/* import vercel from "@astrojs/vercel/serverless"; */
-import vercel from "@astrojs/vercel/static"
+import vercel from "@astrojs/vercel/serverless";
+/* import vercel from "@astrojs/vercel/static" */
 
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: "https://my-portfolio-delta-one-61.vercel.app",
-  output: "static",
+  output: "server",
   adapter: vercel(),
 });
 
